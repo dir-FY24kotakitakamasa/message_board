5020683db1a4a69f51c835034dc89000faad1bf9
dir-FY24kotakitakamasa <takamasa.kotaki.v702@dirbato.co.jp>
2024-04-18 10:00:35 +0900

    getAllMessagesを追加」

--- message_board/src/models/Message.java
+++ message_board/src/models/Message.java
@@ -1,15 +1,24 @@
 package models;
 
+import java.sql.Timestamp;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
-import com.google.protobuf.Timestamp;
+
 
 @Entity
+@NamedQueries({ 
+    @NamedQuery(name = "getAllMessages", query = "SELECT m FROM Message AS m ORDER BY m.id DESC")
+
+})
+
 @Table(name = "messages")
 public class Message {
     @Id
@@ -37,26 +46,34 @@ public class Message {
         this.id = id;
     }
     
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
     public String getContent() {
         return content;
     }
-    
+
     public void setContent(String content) {
         this.content = content;
     }
-    
-    public Timestamp getCreated_at(Timestamp created_at) {
-        return created_at;        
+
+    public Timestamp getCreated_at() {
+        return created_at;
     }
-    
+
     public void setCreated_at(Timestamp created_at) {
         this.created_at = created_at;
     }
-    
+
     public Timestamp getUpdated_at() {
         return updated_at;
     }
-            
+
     public void setUpdated_at(Timestamp updated_at) {
         this.updated_at = updated_at;
     }
