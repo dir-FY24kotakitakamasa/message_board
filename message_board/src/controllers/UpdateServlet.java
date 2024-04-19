4739643d8c7326668a54c113160a76b9437494c1
dir-FY24kotakitakamasa <takamasa.kotaki.v702@dirbato.co.jp>
2024-04-19 11:52:19 +0900

    バリデーションを追加

--- message_board/src/controllers/UpdateServlet.java
+++ message_board/src/controllers/UpdateServlet.java
@@ -2,54 +2,68 @@ package controllers;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import models.Message;
+import models.validators.MessageValidator;
 import utils.DBUtil;
 
 @WebServlet("/update")
 public class UpdateServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     public UpdateServlet() {
         super();
     }
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	    
-	    String _token = request.getParameter("_token");
-	    if (_token != null && _token.equals(request.getSession().getId())) {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String _token = request.getParameter("_token");
+        if (_token != null && _token.equals(request.getSession().getId())) {
             var em = DBUtil.createEntityManager();
-            
-            var m = em.find(Message.class,(Integer)(request.getSession().getAttribute("message_id")));
-            
+
+            var m = em.find(Message.class, (Integer) (request.getSession().getAttribute("message_id")));
+
             var title = request.getParameter("title");
             m.setTitle(title);
-            
+
             var content = request.getParameter("content");
             m.setContent(content);
-            
+
             var currentTime = new Timestamp(System.currentTimeMillis());
             m.setUpdated_at(currentTime);
-            
-            em.getTransaction().begin();
-            em.getTransaction().commit();
-            request.getSession().setAttribute("flush", "更新が完了しました。");
-            em.close();
-            
-            request.getSession().removeAttribute("message_id");
-            
-            response.sendRedirect(request.getContextPath() + "/index");
-           
+
+            List<String> errors = MessageValidator.validate(m);
+            if (errors.size() > 0) {
+                em.close();
+
+                request.setAttribute("_token", request.getSession().getId());
+                request.setAttribute("message", m);
+                request.setAttribute("errors", errors);
+
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
+                rd.forward(request, response);
+            } else {
+
+                em.getTransaction().begin();
+                em.getTransaction().commit();
+                request.getSession().setAttribute("flush", "更新が完了しました。");
+                em.close();
+
+                request.getSession().removeAttribute("message_id");
+
+                response.sendRedirect(request.getContextPath() + "/index");
+
+            }
+
         }
 
-	
-	
-	
-	}
-
+    }
 }
